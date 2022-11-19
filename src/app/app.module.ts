7d20bd6a3441b8a1d2e8ae8701fcd19307b9db5e
unknown <ge49cum@mytum.de>
2022-11-19 22:03:54 +0100

    selfCalculation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +20,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SavingsComponent } from './savings/savings.component';
 import { OwnReceiptComponent } from './own-receipt/own-receipt.component';
 import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -45,7 +47,9 @@ import {MatTableModule} from '@angular/material/table';
     MatChipsModule,
     MatButtonToggleModule,
     ReactiveFormsModule,
-    MatTableModule
+    MatTableModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/own-receipt/own-receipt.component.ts
+++ src/app/own-receipt/own-receipt.component.ts
@@ -1,28 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms'  
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms' 
+import * as groceries from '../../assets/data/lebensmittel.json'; 
+import { CalculationService } from '../services/calculation.service';
 
 @Component({
   selector: 'app-own-receipt',
   templateUrl: './own-receipt.component.html',
   styleUrls: ['./own-receipt.component.scss']
 })
-export class OwnReceiptComponent implements OnInit {
+export class OwnReceiptComponent {
 
   productForm: FormGroup;  
-
-  constructor() { }
+  ingredientsArray :any = []
+  private groceries: any = groceries;
+  private ingriedientsAmountArray: any = []
+  public inflation: Number
   
 
-  ngOnInit(): void {
-  }
-  public addColoumn() {
+  constructor(private fb:FormBuilder, private calculationService: CalculationService) {  
+     
+    this.groceries.groceries.forEach( (element:any) => {
+      this.ingredientsArray.push(element.name)
+    })
 
-  }
-  public removeColoumn(event:any){
-
-  
-  }
+    this.productForm = this.fb.group({  
+      name: '',  
+      quantities: this.fb.array([]) ,  
+    });  
+  }  
+    
   quantities() : FormArray {  
     return this.productForm.get("quantities") as FormArray  
-  } 
-}
+  }  
+     
+  newRow(): FormGroup {  
+    return this.fb.group({  
+      name: '',  
+      internalAmount: 0,  
+    })  
+  }  
+     
+  addRow() {  
+    this.quantities().push(this.newRow());  
+  }  
+     
+  removeRow(i:number) {  
+    this.quantities().removeAt(i);
+    this.calc()  
+  }  
+     
+  onSubmit() {  
+    console.log(this.productForm.value);  
+  }
+  calc(){
+    console.log("he")
+    this.ingriedientsAmountArray= []
+    this.productForm.value.quantities.forEach((element:any) => {
+      this.ingriedientsAmountArray.push(element)
+    });
+    console.log(this.ingriedientsAmountArray)
+    this.inflation =this.calculationService.calculateReceiptSaving(this.ingriedientsAmountArray)
+    if(this.inflation== Number.NEGATIVE_INFINITY)
+    {
+      this.inflation=0
+    }
+    console.log(this.inflation)
+  }
+}  
