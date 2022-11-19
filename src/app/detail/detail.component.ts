6212892c73badb288f02b03e91f7c9b0f53325ab
Primary <primary@w204-6o-v4.eduroam.dynamic.rbg.tum.de>
2022-11-19 15:34:02 +0100

    Savings function, data extension

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReceiptService } from '../services/receipt.service';
+import * as groceries from '../../assets/data/lebensmittel.json';
+import * as inflationRates from '../../assets/data/inflation.json';
 
 @Component({
   selector: 'app-detail',
@@ -14,6 +16,10 @@ export class DetailComponent implements OnInit {
   public portions: number = 4;
   public defaultPortions: number = 4;
   public receipt: any;
+  public saving: string;
+
+  private groceries = groceries;
+  private inflationRates: any = inflationRates;
 
   constructor(
     private receiptService: ReceiptService,
@@ -34,13 +40,54 @@ export class DetailComponent implements OnInit {
     }
   }
 
-  private parseReceipt() {
+  private parseReceipt(): void {
     this.receiptService.getReceipts().subscribe((receipts: any) => {
       let receipt = receipts[this.id];
       this.receipt = receipt;
 
       this.ingridients = receipt.ingridients;
       this.instructions = receipt.instructions;
+
+      this.calculateSaving();
     });
+  }
+
+  private calculateSaving() {
+    let referenceInflation = 19.4;
+
+    let totalPrice = 0;
+    // Sum of all pricePerIngredients
+    for (let ingridient of this.ingridients) {
+      if (ingridient.ignore !== true) {
+        let unitPrice = this.getUnitPrice(ingridient.name).price;
+
+        totalPrice += ingridient.internalAmount * unitPrice;
+      }
+    }
+
+    let inflationSum = 0;
+    for (let ingridient of this.ingridients) {
+      if (ingridient.ignore !== true) {
+        let ingredientReference = this.getUnitPrice(ingridient.name);
+        let unitPrice = ingredientReference.price;
+
+        let weightingFactor = unitPrice / totalPrice;
+
+        let ingredientInflation =
+          this.inflationRates[ingredientReference.category].inflation;
+
+        let weightedInflatedPrice = weightingFactor * ingredientInflation;
+
+        inflationSum += weightedInflatedPrice;
+      }
+    }
+
+    this.saving = (referenceInflation - inflationSum).toFixed(1);
+  }
+
+  private getUnitPrice(name: string): any {
+    return this.groceries.groceries.find(
+      (e) => e.name.toLowerCase() === name.toLowerCase()
+    );
   }
 }
